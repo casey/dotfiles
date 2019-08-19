240375a1d4716b82d5b32fed92f4509ff9ba58f2
Casey Rodarmor <casey@rodarmor.com>
2019-08-19 00:49:25 -0700

    [zsh] Display man pages with color

--- etc/fn.zsh
+++ etc/fn.zsh
@@ -15,6 +15,19 @@ bind:() {
   bindkey -M vicmd "$1" "$2"
 }
 
+function man() {
+  env \
+    LESS_TERMCAP_md=$(tput bold; tput setaf 4) \
+    LESS_TERMCAP_me=$(tput sgr0) \
+    LESS_TERMCAP_mb=$(tput blink) \
+    LESS_TERMCAP_us=$(tput setaf 2) \
+    LESS_TERMCAP_ue=$(tput sgr0) \
+    LESS_TERMCAP_so=$(tput smso) \
+    LESS_TERMCAP_se=$(tput rmso) \
+    PAGER="${commands[less]:-$PAGER}" \
+    man "$@"
+}
+
 arginfo() {
   echo $# arguments:
 
