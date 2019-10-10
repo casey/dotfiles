765b8285f885a4d52f128b78c38b998722bd30e6
Casey Rodarmor <casey@rodarmor.com>
2019-10-10 08:58:58 -0700

    [fish] Make <c-f> a noop in dfault mode

--- etc/fish-functions/fish_user_key_bindings.fish
+++ etc/fish-functions/fish_user_key_bindings.fish
@@ -2,6 +2,7 @@ function fish_user_key_bindings
   fish_hybrid_key_bindings
   fzf_key_bindings
   bind -M insert -m default \cj force-repaint
+  bind -M default -m default \cj force-repaint
 
   bind -M insert \cg fzf-cd-widget
   bind -M insert \cx fzf-history-widget
