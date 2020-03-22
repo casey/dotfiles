cdb02c315cc88c6520f38f56e8d5d16bc4d44a16
Casey Rodarmor <casey@rodarmor.com>
2020-03-22 12:23:46 -0700

    [fish] Show git information in prompt

--- etc/fish-functions/fish_right_prompt.fish
+++ etc/fish-functions/fish_right_prompt.fish
@@ -1,5 +1,32 @@
 function fish_right_prompt
+
 	set_color brblue
   echo -n (prompt_pwd)
   set_color normal
+
+  if __fish_is_git_repository
+    if not git diff --quiet
+      set_color red
+    else if not git diff --cached --quiet
+      set_color green
+    else
+	    set_color brblue
+    end
+
+    echo -n " ⟦"
+
+    git branch --show-current
+
+    if git rev-parse -q --verify refs/stash > /dev/null
+      set stashes (git rev-list --walk-reflogs --count refs/stash)
+
+      if test $stashes -gt 0
+        echo -n "|$stashes"
+      end
+    end
+
+    echo -n "⟧"
+
+    set_color normal
+  end
 end
