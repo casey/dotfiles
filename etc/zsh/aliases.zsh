f75bc9c74a3806a7f4dcdbb16e88b4ceb2782d92
Casey Rodarmor <casey@rodarmor.com>
2020-04-05 07:46:05 -0700

    [zsh] Set up aliases

---
+++ etc/zsh/aliases.zsh
@@ -0,0 +1,44 @@
+## Ⅰ
+alias: g 'git'
+alias: m 'vman'
+alias: j 'just'
+alias: f 'fd'
+alias: p 'python3 -ic "import math, time, random, re, sys, itertools, os, string;"'
+alias: v vim-fzf
+
+## Ⅱ
+alias: .. 'cd ..'
+alias: .j 'j --justfile ~/.justfile --working-directory ~'
+alias: al 'alias-list 2'
+alias: a1 'alias-list 1'
+alias: a2 'alias-list 2'
+alias: ll 'exa --long --binary --group --git'
+alias: la 'exa --across --all'
+alias: gd 'cd "$(tail -n1 ~/.savedir)"'
+alias: gi 'git-info'
+alias: kb 'keybase'
+alias: kc 'keybase-chat'
+alias: ls 'exa'
+alias: zf 'zsh-edit-function'
+alias: p1 'ping 1.1.1.1'
+alias: p8 'ping 8.8.8.8'
+alias: p6 'ping6 2606:4700:4700::1111'
+
+## Ⅲ
+alias: how 'howdoi -a'
+alias: psg 'ps aux | grep'
+alias: rwd 'cd `pwd -P`'
+
+## Ⅳ
+alias: Kill 'kill -9'
+alias: cd.. 'cd ..'
+alias: diff 'colordiff'
+alias: grep 'grep --color=auto'
+alias: help 'run-help'
+alias: pare 'tr -s "[:blank:]" " " | cut -d " " -f'
+alias: port 'port -vuRc'
+alias: sane 'stty sane'
+
+## Ⅴ
+alias: egrep 'egrep --color=auto'
+alias: eject 'drutil tray eject'
