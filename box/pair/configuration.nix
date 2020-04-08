401a6eb60c8e555a3f4e0a7341dbb588f19c0281
Casey Rodarmor <casey@rodarmor.com>
2020-04-08 03:51:36 -0700

    [box/pair] Initialize

---
+++ box/pair/configuration.nix
@@ -0,0 +1,122 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+  boot = {
+    cleanTmpDir = true;
+
+    grub = {
+      enable     = true;
+      device     = "/dev/vda";
+      efiSupport = true;
+      version    = 2;
+    };
+  };
+
+  time.timeZone = "America/Los_Angeles";
+
+  networking = {
+    hostName = "pair";
+    firewall = {
+      enable          = true;
+      allowPing       = true;
+      allowedTCPPorts = [];
+      allowedUDPPorts = [];
+    };
+  };
+
+  i18n = {
+    consoleFont         = "Lat2-Terminus16";
+    defaultLocale       = "en_US.UTF-8";
+    consoleUseXkbConfig = true;
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  services = {
+    mingetty.greetingLine = ''Welcome!'';
+
+    xserver = {
+      layout     = "us";
+      xkbVariant = "altgr-intl";
+      xkbOptions = "ctrl:nocaps, compose:rwin, numpad:mac, shift:both_capslock_cancel, lv3:ralt_switch";
+    };
+
+    sysstat = {
+      enable = true;
+    };
+
+    openssh.enable = true;
+  };
+
+  users.extraUsers.pair = {
+    extraGroups  = [
+      "libvirtd"
+      "uucp"
+      "wheel"
+    ];
+    isNormalUser = true;
+    shell        = pkgs.zsh;
+    uid          = 1000;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPlE0aeKgtJO5hE2SOWPzCOk3ht1mKUPNXUyMK9B7zT4 durandal"
+    ];
+  };
+
+  environment = {
+    systemPackages = with pkgs; [
+      bat
+      bc
+      binutils
+      cargo-edit
+      cargo-watch
+      cmake
+      colordiff
+      dash
+      ed
+      exa
+      fd
+      file
+      fzf
+      gitAndTools.gitull
+      gnumake
+      gnupg
+      gopher
+      htop
+      imagemagick
+      ncurses
+      neofetch
+      nmap
+      parted
+      pkgconfig
+      python38
+      qemu
+      ranger
+      rustup
+      sysstat
+      tcpdump
+      tcptrack
+      telnet
+      tig
+      tmux
+      traceroute
+      unrar
+      unzip
+      urbit
+      vimHuge
+      wget
+      whois
+      ympd
+      zlib
+      zsh
+    ];
+  };
+
+  system.stateVersion = "18.09";
+}
---
+++ box/pair/init.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+#
+# Adapted from https://eipi.xyz/blog/nixos-on-vultr/
+#
+# Install NixOS on a Vultr VPS
+
+set -euxo pipefail
+
+CONFIG_URL='https://raw.githubusercontent.com/casey/local/master/box/pair/configuration.nix'
+
+umount /dev/vda*
+
+# create partitions (with 2G swap)
+(
+echo g
+
+# swap
+echo n
+echo
+echo
+echo +2GB
+echo t
+echo
+echo 19
+
+# bios boot (for grub)
+echo n
+echo
+echo
+echo +16MB
+echo t
+echo
+echo 4
+
+# /
+echo n
+echo
+echo
+echo
+
+echo w
+) | fdisk /dev/vda
+
+fdisk -l /dev/vda
+
+# enable swap
+mkswap -f /dev/vda1
+swapon /dev/vda1
+free -h
+
+# wait
+sleep 5
+
+# create filesystem and mount
+mkfs.ext4 /dev/vda3 -Lroot
+mount /dev/vda3 /mnt
+
+# generate Nixos config
+nixos-generate-config --root /mnt
+
+echo "System configuration.nix:"
+curl "$CONFIG_URL" | tee /mnt/etc/nixos/configuration.nix
+
+# install Nixos
+nixos-install
+
+# unmount
+sync
+umount /dev/vda3
+
+echo "Done. Now reboot via \"Remove ISO\" on the Vultr web UI."
