b857c58d30399e67099b0c1ecb2eaa1557f698f6
Casey Rodarmor <casey@rodarmor.com>
2021-05-27 23:55:05 -0700

    [pkg/overviews] Misc improvements
    - Handle different image formats
    - Skip files that have already been downloaded
    - Print out progress

--- pkg/crates/overviews/src/main.rs
+++ pkg/crates/overviews/src/main.rs
@@ -3,7 +3,7 @@ use reqwest::{
   StatusCode,
 };
 use serde::Deserialize;
-use std::{fs, process::Command};
+use std::{fs, path::Path, process::Command};
 
 const URL: &str = "https://ai7o5ij8d5-dsn.algolia.net/1/indexes/*/queries?&x-algolia-application-id=AI7O5IJ8D5&x-algolia-api-key=7f1a509e834f885835edcfd3482b990c";
 const BODY: &str = r#"{"requests":[{"indexName":"overview","params":"hitsPerPage=1000"}]}"#;
@@ -46,8 +46,27 @@ fn main() {
   let response = serde_json::from_str::<Response>(&text).unwrap();
 
   for result in response.results {
-    for hit in result.hits {
-      eprintln!("Fetching {}…", hit.image.url);
+    for (i, hit) in result.hits.iter().enumerate() {
+      eprintln!(
+        "Fetching {}/{}: {}…",
+        i + 1,
+        result.hits.len(),
+        hit.image.url
+      );
+
+      let extension = match hit.image.url.split('.').last().unwrap() {
+        "jpg" | "jpeg" => "jpg",
+        "png" => "png",
+        "gif" => "gif",
+        unrecognized => panic!("unrecognized: {}", unrecognized),
+      };
+
+      let dst = format!("images/{}.{}", hit.slug, extension);
+
+      if Path::new(&dst).is_file() {
+        eprintln!("Already fetched!");
+        continue;
+      }
 
       let response = get(&hit.image.url).unwrap();
 
@@ -57,21 +76,15 @@ fn main() {
 
       let bytes = response.bytes().unwrap();
 
-      let extension = match hit.image.url.split('.').last().unwrap() {
-        "jpg" | "jpeg" => "jpg",
-        "png" => "png",
-        unrecognized => panic!("unrecognized: {}", unrecognized),
-      };
-
-      let jpg = format!("images/{}.{}", hit.slug, extension);
-      fs::write(&jpg, bytes).unwrap();
+      let dst = format!("images/{}.{}", hit.slug, extension);
+      fs::write(&dst, bytes).unwrap();
 
       let description = format!("{}, {}: {}", hit.title, hit.location, hit.description);
 
       let status = Command::new("exiftool")
         .arg(format!("-exif:imagedescription={}", description))
         .arg("-overwrite_original")
-        .arg(jpg)
+        .arg(dst)
         .status()
         .unwrap();
 
