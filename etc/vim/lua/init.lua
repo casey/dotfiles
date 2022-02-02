2698c3192b26b9c42a6c97342af5516ad296fae8
Casey Rodarmor <casey@rodarmor.com>
2022-02-02 14:59:39 -0800

    [vim] Configure LSP

--- etc/vim/lua/init.lua
+++ etc/vim/lua/init.lua
@@ -1,8 +1,13 @@
 -- configure rust analyzer
-require('lspconfig').rust_analyzer.setup({
-  settings = {
-    ["rust-analyzer"] = {
-      diagnostics = { disabled = { 'inactive-code' } },
-    },
+require('lspconfig').rust_analyzer.setup({})
+
+-- hide diagnostic messages
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics,
+  {
+    virtual_text = false,
+    signs = false,
+    update_in_insert = false,
+    underline = false,
   }
-})
+)
