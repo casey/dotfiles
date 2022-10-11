6a5866722c53c3b8078b32bb4bb1e523c185f2dc
Casey Rodarmor <casey@rodarmor.com>
2022-10-11 13:58:05 -0700

    [vim] Remove comments from init.lua

--- etc/vim/lua/init.lua
+++ etc/vim/lua/init.lua
@@ -1,7 +1,5 @@
--- configure rust analyzer
 require('lspconfig').rust_analyzer.setup({})
 
--- hide diagnostic messages
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics,
   {
